11b1a2b2d72062249f7ede5a55af0a1491d47f8c
zaidkhan3251 <48489044+zaidkhan3251@users.noreply.github.com>
2020-06-22 16:22:42 -0700

    okkk

--- src/transContext.js
+++ src/transContext.js
@@ -1,4 +1,4 @@
-import React, { createContext } from "react"
+import React, { createContext,useReducer } from "react"
 import TransactionReducer from './transReducer';
 
 const initialTransactions = [
